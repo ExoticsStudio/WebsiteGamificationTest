b02e7854291a989ca42364a30bc952929f4f4591
ExoticsIDN <80768769+ExoticsIDN@users.noreply.github.com>
2022-06-23 13:50:44 +0700

    Update V1.6
    Test Fetching API LEADERBOARD

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-const myURL = new URL("http://127.0.0.1:5500/index.html");
+/*const myURL = new URL("http://127.0.0.1:5500/index.html");
             myURL.searchParams.append('x',42);
-            console.log(myURL.search);
+            console.log(myURL.search);*/
+
+          
+          
+          
+          /*  const data = { username: 'example' };
+
+            fetch('https://wsdl.maybankfinance.co.id:8282/api/BackToWork_Main/BackToWork/GetTop_10_Highscore', 
+                    {
+                        method: 'POST', 
+                        headers: {
+                                    'Content-Type': 'application/json',
+                                    'X-Auth-Token':'6GfwBwJcYNtfldKkjScCy2GQJuL6ZAnjFC8KIKa0',
+                                 },
+                        body: JSON.stringify(data),
+                        mode:"cors",
+                        cache:"default",
+                    }
+                )
+
+                .then(response => response.json())
+                .then(data => {console.log('Success:', data);})
+                .catch((error) => {console.error('Error:', error);});
+
+
+*/
+
+            
